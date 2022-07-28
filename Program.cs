62edf36a439db096d0c0456ceee2624fbfddc20d
hlk9 <hoanglong.cmb@gmail.com>
2022-07-28 17:05:38 +0700

    for new game

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
 ﻿Console.WriteLine("Welcome to Dark World!");
-Console.WriteLine("Xin chao mung!\n1: Choi moi\n2: Choi tiep\n3: ");
+Console.WriteLine("Hello please select mode with number below!\n1: New Game\n2: Load Game\n3: Credit\n4: Exit");
+Console.Write("Select>: ");
+string sel = Console.ReadLine();
 
-
+//For new game
+if(sel=="1")
+{
+    Console.WriteLine("Select Class of your Hero!");
+    Console.WriteLine("1: Knight");
+    Console.WriteLine("2: Wizard");
+    Console.WriteLine("3: Elf");
+    Console.WriteLine("4: Warrior");
+     Console.WriteLine("Select>: ");
+    string classHero = Console.ReadLine();
+}
