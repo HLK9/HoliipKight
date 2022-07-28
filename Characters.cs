16d9814c98610a5c5e5d6d65819f351a16a9ac92
Hoang Long <62252259+HLK9@users.noreply.github.com>
2022-07-28 18:00:04 +0700

    Update Characters.cs
    init player stas

---
+++ Characters.cs
@@ -0,0 +1,11 @@
+class Character
+{
+    public int Health {get; set;}
+    public int Damage {get; set;}
+    public int Defend {get; set;}
+    public int Mana {get;set;}
+    public int Level {get;set;}
+    public int EXP {get;set;}
+    public string Class{get;set;}
+
+}
