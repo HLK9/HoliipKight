eab93d5e6d0fba88a1200432311b1b423c057fa4
Hoang Long <62252259+HLK9@users.noreply.github.com>
2022-07-31 19:48:28 +0700

    Update Characters.cs

--- Characters.cs
+++ Characters.cs
@@ -8,5 +8,17 @@ class Character
     public int EXP {get;set;}
     public string Class{get;set;}
     public string UserName{get;set;}
+    public Character(int _health,int _damage, int _defend, int _mana, int _level, int _exp, string _class, string _userName)
+    {
+        Health = _health;
+        Damage=_damage;
+        Defend = _defend;
+        Mana = _mana;
+        Level = _level;
+        EXP = _exp;
+        Class = _class;
+        UserName = _userName;
+        
+    }
 
 }
